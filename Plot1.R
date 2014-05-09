69cb8206263c236ebc9ef86d0845e89532a5b335
manvindra singh <manvindra.singh@gmail.com>
2014-05-09 20:46:52 +0530

    Plot 1 for project 1

---
+++ Plot1.R
@@ -0,0 +1,13 @@
+setwd("C:/Users/manu/Documents/coursera")
+data1<-read.csv("household_power_consumption.txt",sep=";",header=T)
+head(data1)
+data1$Date<-as.Date(data1$Date,"%d/%m/%Y")
+
+data2<-subset(data1, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+head(data2)
+Global_active_power<-as.numeric(as.character(Global_active_power))
+data2$uniDate <- as.POSIXct(paste(Date, Time), format="%Y-%m-%d %H:%M:%S")
+attach(data2)
+hist(Global_active_power,col="red",xlab="Global Active Power (killowatts)",main="Global Active Power")
+dev.copy(png,file="Plot1.png")
+dev.off()
